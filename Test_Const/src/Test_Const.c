4d135fd8509a4d9c1b593996bbb21281b38e2984
HuyChung <35482473+HuyChung@users.noreply.github.com>
2018-08-03 18:09:26 +0900

    Test

--- Test_Const/src/Test_Const.c
+++ Test_Const/src/Test_Const.c
@@ -11,7 +11,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int test();
+
+unsigned int Send_Data = 150;
+unsigned int Received_Data;
+
 int main(void) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	test();
+
 	return EXIT_SUCCESS;
 }
+
+int test() {
+	const int x = 3;
+
+	printf("%d\n", x);
+
+	return x;
+}
+
+void WriteData_fun(){
+	unsigned int data = 0;
+
+}
+
+void ReadData_fun(unsigned int *readData){
+	*readData = Send_Data;
+}
+
