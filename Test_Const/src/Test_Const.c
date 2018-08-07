f585f924f29bfeb0e92dcf13b71b5f62c8c9b307
HuyChung <35482473+HuyChung@users.noreply.github.com>
2018-08-07 16:00:40 +0900

    Test truyen vao con tro hang: const int *x

--- Test_Const/src/Test_Const.c
+++ Test_Const/src/Test_Const.c
@@ -11,62 +11,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int test();
-void ReadData_func(unsigned int *readData);
+unsigned int Buffer_Data;
 
-unsigned int Send_Data;
-unsigned int Received_Data;
+const int *Buffer_Pointer_Data;
 
 int main(void) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 
 	// Test truyen tham chieu trong C
-	int read_Data = 0;
 	int send_Data = 100;
+	int read_Data = 0;
+
 	WriteData_func(send_Data);
 	ReadData_func(&read_Data);
 	puts("!!!Truyen tham chieu trong C!!!");
+	printf("send_Data = %d\n", send_Data);
 	printf("read_Data = %d\n", read_Data);
 
 	// Test truyen con tro hang(pointer to constant const int *px) trong C
 	puts("!!!pointer to constant - const int *px!!!");
 	test_PointerToConstant();
 
-	// Test truyen con tro hang(pointer to constant const int *px) trong C
-	puts("!!!pointer to constant version 2 - const int **p_data!!!");
-	test_PointerToConstant_v2();
+	// Test truyen vao con tro hang(pointer to constant: const int *px) trong C
+	puts("!!!Test truyen vao con tro hang - const int *p_data!!!");
+	const int *send_Pointer_Data = 789;
+	const int *read_Pointer_Data = 0;
+
+	Write_PointerToConstant(send_Pointer_Data);
+	Read_PointerToConstant(&read_Pointer_Data);
+	printf("send_Pointer_Data = %d\n", send_Pointer_Data);
+	printf("read_Pointer_Data = %d\n", read_Pointer_Data);
 
 	return EXIT_SUCCESS;
 }
 
+// Ghi du lieu vao bo dem Buffer_Data
 void WriteData_func(unsigned int sendData){
-	Send_Data = sendData;
+	Buffer_Data = sendData;
 }
 
+//Doc du lieu vao bo dem Buffer_Data
 void ReadData_func(unsigned int *readData){
-	*readData = Send_Data;
+	*readData = Buffer_Data;
 }
 
 void test_PointerToConstant(){
 	int x = 500;
-	const int *px;
+	int y = 200;
+	const int *px; // khong the sử dụng dereference pointer để thay đổi giá trị của "x" hay "y"
 
-	px = &x;
-	printf("px=%d\n", px);
+	px = &x;       // => Khong the khai bao *px = 200; duoc
+	printf("px=%p\n", px);
+	printf("*px=%d\n", *px);
+
+	px = &y;
+	printf("px=%p\n", px);
 	printf("*px=%d\n", *px);
 }
 
-void test_PointerToConstant_v2(){
-	int x = 1000;
-	const int **p_data; //Gia tri ben trong cua **p_data khong the thay doi hay **p_data khong the su dung
+// Ghi du lieu vao bo dem Buffer_Pointer_Data
+void Write_PointerToConstant(const int *sendData){
+	Buffer_Pointer_Data = sendData;
+}
 
-
-	*p_data = x;
-
-	printf("%d\n", p_data);
-	printf("%d\n", &*p_data);
-	printf("%d\n", &(**p_data));
-
+// Doc du lieu tu bo dem Buffer_Pointer_Data
+void Read_PointerToConstant(const int **readData){
+	*readData = Buffer_Pointer_Data;
 }
 
 
